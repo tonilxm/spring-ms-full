6b812fa132bffa28834d3a6b1c701ddd6d9cfb2a
Toni <toni.xm@gmail.com>
2022-03-19 22:38:32 +0700

    Initial commit

---
+++ service-registry/src/test/java/com/toni/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toni.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
